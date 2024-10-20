57dd94f7cd77ae9181d05005500198d007e95403
Patrick Mwangi <pwmwangi2@gmail.com>
2024-10-20 20:53:17 +0300

    solved sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+  if (num1 < 0 || num2 < 0 || !Number.isInteger(num1) || !Number.isInteger(num2)) {
+    return "ERROR";
+  }
+  if (num2 < num1) {
+    [num1, num2] = [num2, num1];
+  }
+  let numberOfTerms =num2 -num1 + 1;
+  const sum = (numberOfTerms/2) * (num1 + num2);
+  return sum;
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+
